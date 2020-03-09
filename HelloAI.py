eec22e8b4e81cc37884ac4d69b2339c7907f26ae
Elijah Kelly <ekelly14@ymail.com>
2020-03-09 10:09:21 -0500

    Implement Greedy Best First Algorithm

--- HelloAI.py
+++ HelloAI.py
@@ -1,15 +1,25 @@
 import sys
-import copy
+import heapq
+from random import seed
+from random import randint
 from timeit import default_timer as timer
 
 class GameState:
 
   def __init__(self, inputGrid, inputPool):
     self.gameGrid = inputGrid   #the 2d array representing the game board
-    self.x = len(inputGrid[0])  #the height of the board
-    self.y = len(inputGrid)     #the width of the board 
+    self.x = len(inputGrid[0])-1  #the height of the board
+    self.y = len(inputGrid)-1     #the width of the board 
     self.spawnPool = inputPool  #The array of the spawn pattern
     self.processString = ""     #A string keeping track of the path
+    self.weight = 0
+  def __lt__(self, other):
+    return self.weight > other.weight
+
+  def fast_copy(self):
+    copy_object = GameState([list(x) for x in self.gameGrid], [x for x in self.spawnPool])
+    copy_object.processString = self.processString
+    return copy_object
 
   #Generate a new tile in the grid
   def generateTile(self):
@@ -19,16 +29,16 @@ class GameState:
       self.gameGrid[0][0] = self.spawnPool[0]
 
     #check top right
-    elif self.gameGrid[0][self.x-1] == 0:
-      self.gameGrid[0][self.x-1] = self.spawnPool[0]
+    elif self.gameGrid[0][self.x] == 0:
+      self.gameGrid[0][self.x] = self.spawnPool[0]
 
     #check bottom right
-    elif self.gameGrid[self.x-1][self.y-1] == 0:
-      self.gameGrid[self.x-1][self.y-1] = self.spawnPool[0]
+    elif self.gameGrid[self.y][self.x] == 0:
+      self.gameGrid[self.y][self.x] = self.spawnPool[0]
 
     #check bottom left
-    elif self.gameGrid[0][self.y-1] == 0:
-      self.gameGrid[0][self.y-1] = self.spawnPool[0]
+    elif self.gameGrid[self.y][0] == 0:
+      self.gameGrid[self.y][0] = self.spawnPool[0]
 
     #spawning fails
     else:
@@ -52,26 +62,28 @@ class GameState:
 
   #Perform a left shift on the game grid
   def leftShift(self):
-    tempState = copy.deepcopy(self.gameGrid)
-    for i in range(0, self.y):
+    tempState = self.fast_copy()
+    for i in range(0, self.y+1):
       #remove zeros form array
       self.gameGrid[i] = list(filter(lambda num: num != 0, self.gameGrid[i]))
       #combine values 
       k = self.combineValues(self.gameGrid[i])
       #reapply values
-      for j in range(0, (self.y)-(len(self.gameGrid[i]))):
+      for j in range(0, (self.x)-(len(self.gameGrid[i])-1)):
         self.gameGrid[i].append(0)
-    if self.gameGrid == tempState:
-      return
+    if self.gameGrid == tempState.gameGrid:
+      return False
     #Spawn new tile
     self.generateTile()
+    self.getWeight()
     #Add to the process string
     self.processString += "L"
+    return True
 
   #Perform a right shift on the grid
   def rightShift(self):
-    tempState = copy.deepcopy(self.gameGrid)
-    for i in range(0, self.y):
+    tempState = self.fast_copy()
+    for i in range(0, self.y+1):
       #remove zeros from list
       self.gameGrid[i] = list(filter(lambda num: num != 0, self.gameGrid[i]))
       #reverse so the same combine function can be used
@@ -80,22 +92,24 @@ class GameState:
       #re-reverse
       self.gameGrid[i].reverse()
       #replace values
-      for j in range(0, (self.y)-(len(self.gameGrid[i]))):
+      for j in range(0, (self.x)-(len(self.gameGrid[i])-1)):
         self.gameGrid[i].insert(0, 0)
-    if self.gameGrid == tempState:
-      return
+    if self.gameGrid == tempState.gameGrid:
+      return False
     #Spawn new tile
     self.generateTile()
+    self.getWeight()
     #Add process to the path string
     self.processString += "R"
+    return True 
 
   #perform an up shift on the entire grid
   def upShift(self):
-    tempState = copy.deepcopy(self.gameGrid)
-    for i in range(0, self.x):
+    tempState = self.fast_copy()
+    for i in range(0, self.x+1):
       downarr = []
       #formulate an array from the columns
-      for j in range(0, self.y):
+      for j in range(0, self.y+1):
         downarr.append(self.gameGrid[j][i])
         #remove zeros
       downarr = list(filter(lambda num: num != 0, downarr))
@@ -103,51 +117,56 @@ class GameState:
       downarr.reverse()
       k = self.combineValues(downarr)
       #reinsert the zeros
-      for j in range(0, (self.y)-(len(downarr))):
+      for j in range(0, (self.y)-(len(downarr)-1)):
         downarr.insert(0, 0)
       #re-reverse
       downarr.reverse()
       #reinsert values to grid
-      for j in range(0, self.y):
+      for j in range(0, self.y+1):
         self.gameGrid[j][i] = downarr[j]
-    if self.gameGrid == tempState:
-      return
+    
+    if self.gameGrid == tempState.gameGrid:
+      return False
     #Spawn new tile
     self.generateTile()
+    self.getWeight()
     #Append the process to the processString
     self.processString += "U"
+    return True
   
   #perform a down shift on the grid
   def downShift(self):
-    tempState = copy.deepcopy(self.gameGrid)
-    for i in range(0, self.x):
+    tempState = self.fast_copy()
+    for i in range(0, self.x+1):
       downarr = []
       #formulate an array with the correct column values
-      for j in range(0, self.y):
+      for j in range(0, self.y+1):
         downarr.append(self.gameGrid[j][i])
       #remove zeros
       downarr = list(filter(lambda num: num != 0, downarr))
       #combine values
       k = self.combineValues(downarr)
       #readd zeros
-      for j in range(0, (self.y)-(len(downarr))):
+      for j in range(0, (self.y)-(len(downarr)-1)):
         downarr.insert(0, 0)
       #reapply values to grid
-      for j in range(0, self.y):
+      for j in range(0, self.y+1):
         self.gameGrid[j][i] = downarr[j]
-    if self.gameGrid == tempState:
-      return
+    if self.gameGrid == tempState.gameGrid:
+      return False
     #Spawn new tile
     self.generateTile()
+    self.getWeight()
     #Append the process to the processString
     self.processString += "D"
+    return True
   
   #check the win condiditon of the game
   def isWin(self):
     #check the entire grid for the target value
-    for x in range(0, self.x):
-      for y in range(0, self.y):
-        if(self.gameGrid[x][y] == targetNum):
+    for r in self.gameGrid:
+      for i in r:
+        if(i == targetNum):
           return True;
     #else, you have not won (yet!)
     return False
@@ -159,6 +178,65 @@ class GameState:
       for i in r:
         print(i, end=' ')
       print()
+  
+  def getWeight(self):
+    #crappy h(x), change later
+    score = 0
+    for r in self.gameGrid:
+      for i in range(0, len(r)):
+        score += r[i]
+    # Heuristic:
+    # Sum of every tile on the board, divided by the number
+    #   of available tile spaces. This is similar to the 2048 
+    #   scoring system, which gives points based on combined 
+    #   tile values. I multiply this by a psudorandom number
+    #   (seeded value) to prevent very similar boards from
+    #   creating a loop (such as [UDUDUDUDU]) and when 0,
+    #   trims the nodes to be tried for a solution.
+    self.weight = (score/(self.x+1 * self.y+1)) * randint(0,5)
+
+def GreedyBest(state):
+  #make a priority Queue
+  pq = []
+  heapq.heappush(pq, state)
+
+  #As long as there are still valid moves on the queue
+  while(len(pq) != 0):
+    #Pop the one with the highest weight
+    node = heapq.heappop(pq)
+    #If it is a winning state, return it
+    if node.isWin():
+      return node
+    #Else generate the child states, push it to the queue
+    #then continue
+    else:      
+      #the left shift child
+      leftState = node.fast_copy()
+      #If it is a valid move, add state to heapq
+      if(leftState.leftShift()):
+        heapq.heappush(pq, leftState)
+
+      #the right shift child
+      rightState = node.fast_copy()
+      #If it is a valid move, add state to heapq
+      if(rightState.rightShift()):
+        heapq.heappush(pq, rightState)
+
+      #the down shift child
+      downState = node.fast_copy()
+      #If it is a valid move, add state to heapq
+      if(downState.downShift()):
+        heapq.heappush(pq, downState)
+
+      #the up shift child
+      upState = node.fast_copy()
+      #If it is a valid move, add state to heapq
+      if(upState.upShift()):
+        heapq.heappush(pq, upState)
+
+      #Begin While loop again
+
+  print("No Possible Solutions.")
 
 #open the input file
 f = open(sys.argv[1])
@@ -187,37 +265,24 @@ for i in range(0, height):
 #Create a game object with the input
 game = GameState(tempgrid, pattern)
 
-#BFTS Algorithm implementation
+#Get Start time
 start = timer()
 fullstart = start
-isSolved = False
-queue = []
-while(not isSolved):
-  isSolved = game.isWin()
-  if(isSolved):
-    break
 
-  leftGame = copy.deepcopy(game)
-  leftGame.leftShift()
-  queue.append(leftGame)
+seed(1)
+#Run algorithm
+if(len(sys.argv) == 3):
+  print("RGreedyBest Not Implemented")
+else:
+  #Run Greedy Best algorithm
+  finalNode = GreedyBest(game)
 
-  rightGame = copy.deepcopy(game)
-  rightGame.rightShift()
-  queue.append(rightGame)
-
-  downGame = copy.deepcopy(game)
-  downGame.downShift()
-  queue.append(downGame)
-
-  upGame = copy.deepcopy(game)
-  upGame.upShift()
-  queue.append(upGame)
-
-  game = queue[0]
-  queue.pop(0)
-
-
-if(isSolved):
+  #Get finish time
   end = timer()
+
+  #Output results
   print(int(1000 * (end - fullstart)))
-  game.printState()
+  if(finalNode is not None):
+    finalNode.printState()
+  else:
+    pass
